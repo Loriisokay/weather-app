61f59614086be5d2995b3845126dda559a791110
Larissa <lee.lariss94@gmail.com>
2023-02-12 15:13:53 +0600

    fix&feat: try to fix img again + start writitng script

---
+++ script.js
@@ -0,0 +1,16 @@
+function main() {
+    const temperature = document.getElementById('temperature');
+    const city = document.getElementById('city');
+    const time = document.getElementById('time');
+    const date = document.getElementById('date');
+    const conditions = document.getElementById('conditions');
+    const wind = document.getElementById('wind');
+    const humidity = document.getElementById('humidity');
+    const pressure = document.getElementById('pressure');
+
+
+}
+
+
+function 
+
