6451c4e730473976914d44a227e9bcf2f522a93a
Larissa <lee.larissa94@gmail.com>
2023-03-09 22:28:01 +0600

    fix: fix the path problem(7)

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ async function getWeatherData(domain, key, place) { // get API data
     try {
         const response = await fetch(`https://${domain}/v1/current.json?key=${key}&q=${place}&aqi=yes`, {
             method: 'GET',
-            referrerPolicy: "unsafe_url",
+            referrerPolicy: 'no-referrer',
             headers: { 
                 'accept': 'application/dns-json',
             }
